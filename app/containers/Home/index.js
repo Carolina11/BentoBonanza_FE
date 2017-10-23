f323a416a972426437740936d73d5eedcbaa7ba1
Ed <Ed@Eds-MacBook-Air.local>
2017-10-23 16:03:49 -0400

    css started

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -10,14 +10,32 @@ import Helmet from 'react-helmet';
 import './style.css';
 import './styleM.css';
 
+
+
 export default class Home extends React.PureComponent {
   render() {
     return (
       <div className="container">
         <Helmet title="Home" meta={[ { name: 'description', content: 'Description of Home' }]}/>
-        Hi
-        //Remove this line and you can start writing your code here.
-      </div>
+
+        <div className="outLineDiv">
+          <div className="leftSide">
+            <div className="topLeft">
+            </div>
+            <div className="bottomLeft">
+            </div>
+          </div>
+          <div className="rightSide">
+            <div className="fullRight">
+            <div className="sakuraTopLeft01"></div>
+            <div className="sakuraTopLeft02"></div>
+            <h1>bento<br/>bonanza</h1>
+            </div>
+          </div>
+
+        </div>/* off outLineDiv */
+
+      </div>/* off container */
     );
   }
 }
